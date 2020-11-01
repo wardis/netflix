84f14e1a5031e5aac9a6cbe65424a92b5241ed95
wardis <wardi2976@live.fr>
2020-11-01 03:44:00 +0000

    add faq with accordion

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ const { JumbotronContainer } = require('../containers/jumbotron');
 
 export { default as Jumbotron } from './jumbotron';
 export { default as Footer } from './footer';
+export { default as Accordion } from './accordion';
